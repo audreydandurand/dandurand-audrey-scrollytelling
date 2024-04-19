9a762e25c9166924f5446798a8ba665ffa535df9
audreydandurand <112189528+audreydandurand@users.noreply.github.com>
2024-04-19 09:03:35 -0400

    sprite_sheets

--- script/script.js
+++ script/script.js
@@ -3,3 +3,16 @@ gsap.to(".icone", {
     y: '40',
     repeat: -1,
   });
+
+const bodySpriteSheets = document.querySelector(".body-sprite-sheets");
+
+let delai;
+
+window.addEventListener("scroll", function (e) {
+  bodySpriteSheets.classList.add('is-scrolling');
+  window.clearTimeout(delai)
+  delai =  setTimeout(function() {
+    bodySpriteSheets.classList.remove('is-scrolling')
+  }, 100); 
+})
+
