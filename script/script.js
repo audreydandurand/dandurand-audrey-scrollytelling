9cdb28b2c06359587a89ba51ac94daaaec00143b
audreydandurand <112189528+audreydandurand@users.noreply.github.com>
2024-05-05 15:05:25 -0400

    activation_chapitre_scrolTrigger

--- script/script.js
+++ script/script.js
@@ -3,22 +3,41 @@ gsap.registerPlugin(ScrollTrigger);
 /*----- Header -----*/
 
 gsap.to(".icone", { 
-    duration: 4,
-    y: '40',
-    repeat: -1,
-  });
+  scrollTrigger: 'header',
+  duration: 4,
+  y: '40',
+  repeat: -1,
+});
 
 /*----- Chapitre 01 -----*/
 
-gsap.timeline({repeat: 5})
+gsap.timeline({
+  repeat: 5,
+  scrollTrigger: {
+    markers: true,
+    toggleActions: "restart complete reverse reset",
+    trigger: "#chapitre-01"}
+  })
   .from("#chapitre-01 .planete", { duration: 10, scale: 0})
   .to("#chapitre-01 .planete", { duration: 5, x: "-150%", y: "120%",  scale: 1.2});
 
-gsap.timeline({repeat: 5})
+gsap.timeline({
+  repeat: 5,
+  scrollTrigger: {
+    markers: true,
+    toggleActions: "restart complete reverse reset",
+    trigger: "#chapitre-01"}
+  })
   .from("#chapitre-01 .chat-orange", { duration: 10, x: "100%", y: "100%"})
   .to("#chapitre-01 .chat-orange", { duration: 5, x: "-100%", y: "-80%", scale: 1.4});
 
-gsap.timeline({repeat: 5})
+gsap.timeline({
+  repeat: 5,
+  scrollTrigger: {
+    markers: true,
+    toggleActions: "restart complete reverse reset",
+    trigger: "#chapitre-01"}
+  })
   .from("#chapitre-01 .cercle-grand", { scale: 0.7, duration: 2.5})
   .from("#chapitre-01 .cercle-petit", { scale: 0, duration: 3.75})
   .from("#chapitre-01 .cercle-moyen", { scale: 0.5, duration: 3.75} )
@@ -27,6 +46,8 @@ gsap.timeline({repeat: 5})
 /*----- Chapitre 03 et 4 -> Bulles -----*/
 
 gsap.to(".bulles", { 
+  scrollTrigger: '#chapitre3',
+  scrollTrigger: '#chapitre4',
   duration: 10,
   y: '5%',
   scale: 1.2,
@@ -50,6 +71,7 @@ gsap.to(".bulles", {
 /*----- Chapitre 04 -----*/
 
 gsap.from("#chapitre-04 .coraux-rose-mauve", { 
+  scrollTrigger: '#chapitre4',
   duration: 5,
   rotate: '6',
   repeat: -1,
@@ -60,6 +82,7 @@ gsap.from("#chapitre-04 .coraux-rose-mauve", {
 /*----- Chapitre 05 -----*/
 
 gsap.to("#chapitre-05 .hippocampe", { 
+  scrollTrigger: '#chapitre5',
   duration: 4,
   y: '10%',
   repeat: -1,
@@ -67,6 +90,7 @@ gsap.to("#chapitre-05 .hippocampe", {
 });
 
 gsap.from("#chapitre-05 .coraux-vert-rose", { 
+  scrollTrigger: '#chapitre5',
   duration: 5,
   rotate: '3',
   repeat: -1,
@@ -77,6 +101,7 @@ gsap.from("#chapitre-05 .coraux-vert-rose", {
 /*----- Chapitre 06 -----*/
 
 gsap.to("#chapitre-06 .personnage-mauve", { 
+  scrollTrigger: '#chapitre6',
   duration: 4,
   y: '10%',
   repeat: -1,
@@ -85,13 +110,20 @@ gsap.to("#chapitre-06 .personnage-mauve", {
 });
 
 gsap.from("#chapitre-06 .personnage-mauve", { 
+  scrollTrigger: '#chapitre6',
   duration: 10,
   x: '300%',
 });
 
 /*----- Chapitre 08 -----*/
 
-gsap.timeline({repeat: 5})
+gsap.timeline({
+  repeat: 5,
+  scrollTrigger: {
+    markers: true,
+    toggleActions: "restart complete reverse reset",
+    trigger: "#chapitre-08"}
+  })
   .from("#chapitre-08 .cercle-petit-01-morphing", { scale: 0, duration: 3.75})
   .from("#chapitre-08 .cercle-petit-03-morphing", { scale: 0, duration: 5})
   .to("#chapitre-08 .cercle-moyen-01-morphing", { scale: 0, duration: 3.75})
@@ -99,6 +131,7 @@ gsap.timeline({repeat: 5})
   .from("#chapitre-08 .cercle-moyen-01-morphing", { scale: 0, duration: 10});
 
 gsap.from("#chapitre-08 .cercle-grand-01-morphing", { 
+  scrollTrigger: '#chapitre8',
   duration: 8,
   scale: 0.7,
   repeat: -1,
@@ -106,6 +139,7 @@ gsap.from("#chapitre-08 .cercle-grand-01-morphing", {
 });
 
 gsap.from("#chapitre-08 .cercle-grand-02-morphing", { 
+  scrollTrigger: '#chapitre8',
   duration: 10,
   scale: 0.5,
   repeat: -1,
